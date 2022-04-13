d1aa3ea1ca2d3f912cf71839bf462cbcd3f83031
Austin Parker <austincparker@gmail.com>
2022-04-12 19:37:50 -0500

    hotdog model, repo, and interface made

--- JoesHotDogs/Repos/IHotDogRepository.cs
+++ JoesHotDogs/Repos/IHotDogRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace JoesHotDogs.Repos
+﻿using JoesHotDogs.Models;
+
+namespace JoesHotDogs.Repos
 {
     public interface IHotDogRepository
     {
+        public interface IHotDogRepository
+        {
+            List<HotDog> GetAllHotDogs();
+            HotDog GetHotDogById(int id);
+        }
     }
 }
