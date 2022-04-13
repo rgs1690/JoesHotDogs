cd50878cac48e8428f9445c42f9c1217ef60ec9d
Grace Sutherland <rgs1690@gmail.com>
2022-04-12 19:18:35 -0500

    added properties to order class

--- JoesHotDogs/Models/Order.cs
+++ JoesHotDogs/Models/Order.cs
@@ -1,10 +1,19 @@
 ﻿namespace JoesHotDogs.Models
 {
-    public class Orders
+    public class Order
     {
         public string Id { get; set; }
         public string UserId { get; set; }
         public int Total { get; set; }
-
+        public bool Delivery { get; set; }
+        public int CardNum { get; set; }
+        public string Expiration { get; set; }
+        public string NameOnCard { get; set; }
+        public int BillingZip {get; set;}
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
     }
+   
 }
