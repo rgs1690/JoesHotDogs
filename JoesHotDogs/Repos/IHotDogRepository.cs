6533c92746680a51245ea8166e50dfd230367a57
Austin Parker <austincparker@gmail.com>
2022-04-16 12:56:33 -0500

    fixed irepo

--- JoesHotDogs/Repos/IHotDogRepository.cs
+++ JoesHotDogs/Repos/IHotDogRepository.cs
@@ -4,10 +4,7 @@ namespace JoesHotDogs.Repos
 {
     public interface IHotDogRepository
     {
-        public interface IHotDogRepository
-        {
             List<HotDog> GetAllHotDogs();
             HotDog GetHotDogById(int id);
-        }
     }
 }
