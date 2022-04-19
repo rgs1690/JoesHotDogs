b795b0d7c6ca21836da0ea100c56bde10ae8e68e
Joe Gibbs <joseph.gibbs1188@gmail.com>
2022-04-18 21:02:10 -0500

    conflict res again

--- JoesHotDogs/Program.cs
+++ JoesHotDogs/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 <<<<<<< HEAD
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IHotDogRepository, HotDogRepository>();
 =======
