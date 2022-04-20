b2c3a38283ee0e743b2d03a70121dd2a79851e95
darioperez1415 <darioperez1415@gmail.com>
2022-04-19 20:45:50 -0500

    pulled main

--- JoesHotDogs/Models/HotDogOrder.cs
+++ JoesHotDogs/Models/HotDogOrder.cs
@@ -3,7 +3,7 @@
     public class HotDogOrder
     {
         public int Id { get; set; }
-        public string hotdogID { get; set; }
+        public string hotdogId { get; set; }
         public string orderId { get; set; }
     }
 }
