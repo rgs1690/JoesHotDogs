9f463064783a886d2fb49c9369532c0794f647e8
Joe Gibbs <joseph.gibbs1188@gmail.com>
2022-04-23 13:15:48 -0500

    HotDogCards built

--- joeshotdogsclient/src/components/HotDogCards.js
+++ joeshotdogsclient/src/components/HotDogCards.js
@@ -1,11 +1,26 @@
-<!DOCTYPE html>
+import React from 'react';
+import PropTypes from 'prop-types';
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <title></title>
-</head>
-<body>
+export default function HotDogCards({ hotDog }) {
+    return
+    (
+        <>
+            <div class="hotDogCard" style="width: 18rem;">
+                <img src={hotDog.imageUrl} class="card-img-top" alt={hotDog.name}></img>
+                    <div class="card-body">
+                    <h5 class="card-title">{hotDog.name}</h5>
+                    <p class="card-text">{hotDog.description}</p>
+                        <a href="#" class="btn btn-primary">Add to Order?</a>
+                    </div>
+            </div>
+        </>
+    )
+}
 
-</body>
-</html>
+HotDogCards.propTypes = {
+    hotDog: PropTypes.shape({
+        name: PropTypes.string,
+        description: PropTypes.string,
+        imageUrl: PropTypes.string,
+    })
+};
