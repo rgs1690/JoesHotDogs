21da00c3f27fc6dd891233200bcb3694d589eae2
Austin Parker <austincparker@gmail.com>
2022-04-23 13:12:00 -0500

    added navbar

--- joeshotdogsclient/src/App.js
+++ joeshotdogsclient/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routing from './routes';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routing />
       
     </div>
