1d1cf66da53535b7141824a1e6b68e66f029f8de
Joe Gibbs <joseph.gibbs1188@gmail.com>
2022-04-25 19:06:07 -0500

    app.js update

--- joeshotdogsclient/src/App.js
+++ joeshotdogsclient/src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Routing from './routes';
 import Navbar from './components/Navbar';
 import './App.css';
-import Navbar from './components/Navbar';
 
 
 function App() {
-
   return (
     <div>
       <Navbar />
