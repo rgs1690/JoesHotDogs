ec871615937ea7ceea62c8a68dc2948af92f16f8
Grace Sutherland <rgs1690@gmail.com>
2022-04-26 18:34:58 -0500

    start

--- joeshotdogsclient/src/components/OrderForm.js
+++ joeshotdogsclient/src/components/OrderForm.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory, useParams } from 'react-router-dom';
 
-
- rsf
+ 
+ export default function OrderForm() {
+   return (
+     <div></div>
+   )
+ }
+ 
 
