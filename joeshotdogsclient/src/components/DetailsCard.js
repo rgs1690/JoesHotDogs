ce70845f2bcaa855b7a16861215dedb9b3c7fcec
Austin Parker <austincparker@gmail.com>
2022-04-26 19:32:12 -0500

    fix imageurl

--- joeshotdogsclient/src/components/DetailsCard.js
+++ joeshotdogsclient/src/components/DetailsCard.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function HotDogDetailsCard({ hotdog }) {
   return (
 <div className="card mb-3">
-  <img src={hotdog.imageurl} className="card-img-top" alt={hotdog.name} />
+  <img src={hotdog.imageUrl} className="card-img-top" alt={hotdog.name} />
   <div className="card-body">
     <h5 className="card-title">{hotdog.name}</h5>
     <p className="card-text">{hotdog.description}</p>
--- joeshotdogsclient/src/components/HotDogCards.js
+++ joeshotdogsclient/src/components/HotDogCards.js
@@ -11,7 +11,7 @@ export default function HotDogCards({ hotDog }) {
     return (
         <>
             <div className="hotDogCard" style={{ width: '18rem' }}>
-                <img src={hotDog.imageurl} className="card-img-top" alt={hotDog.name}></img>
+                <img src={hotDog.imageUrl} className="card-img-top" alt={hotDog.name}></img>
                     <div className="card-body">
                     <h5 className="card-title">{hotDog.name}</h5>
                     <p className="card-text">{hotDog.description}</p>
