c1d37bb4425b429fe599204e7632df968ef60aa9
Austin Parker <austincparker@gmail.com>
2022-04-26 19:27:53 -0500

    details working

--- joeshotdogsclient/src/components/HotDogCards.js
+++ joeshotdogsclient/src/components/HotDogCards.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function HotDogCards({ hotDog }) {
-    const history = useHistory();
+    const navigate = useNavigate();
     const handleInfo = () => {
-        history.push(`/hotdogs/${hotDog.id}`)
+        navigate(`/hotdogs/${hotDog.id}`)
     }
     return (
         <>
             <div className="hotDogCard" style={{ width: '18rem' }}>
-                <img src={hotDog.imageUrl} className="card-img-top" alt={hotDog.name}></img>
+                <img src={hotDog.imageurl} className="card-img-top" alt={hotDog.name}></img>
                     <div className="card-body">
                     <h5 className="card-title">{hotDog.name}</h5>
                     <p className="card-text">{hotDog.description}</p>
-                    <button type="button" class="btn btn-info" onclick="handleInfo()">Info</button>
+                    <button type="button" className="btn btn-info" onClick={handleInfo}> Info</button>
                         <a href="#" className="btn btn-primary">Add to Order?</a>
                     </div>
             </div>
