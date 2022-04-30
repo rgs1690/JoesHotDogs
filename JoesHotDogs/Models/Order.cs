df50a5c08ef36b79e0caa3862af9ec1f8dbf1e9f
Grace Sutherland <rgs1690@gmail.com>
2022-04-30 12:19:30 -0500

    changed all ints to string in the orderRepo in the backend

--- JoesHotDogs/Models/Order.cs
+++ JoesHotDogs/Models/Order.cs
@@ -6,12 +6,12 @@
         public int UserId { get; set; }
         public int Total { get; set; }
         public bool Delivery { get; set; }
-        public int CardNum { get; set; }
+        public string CardNum { get; set; }
         public string Expiration { get; set; }
         public string NameOnCard { get; set; }
         public int BillingZip {get; set;}
         public string Address { get; set; }
-        public int Phone { get; set; }
+        public string Phone { get; set; }
         public string Date { get; set; }
         public bool Status { get; set; }
     }
