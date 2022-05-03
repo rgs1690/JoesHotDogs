dc4d92d8f73811ed93e88bb5d7f598a902a44c3a
darioperez1415 <darioperez1415@gmail.com>
2022-05-02 21:15:22 -0500

    Refactored orders to use components to call orders

--- joeshotdogsclient/src/components/OrderCard.js
+++ joeshotdogsclient/src/components/OrderCard.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import Proptypes from 'prop-types';
+import {useNavigate} from 'react-router-dom'
 import { Link } from 'react-router-dom';
 import { deleteOrder, updateOrder } from '../api/orderData';
 
-export default function OrderCard() {
+export default function OrderCard({order, setOrders}) {
     const [checked, setChecked] = useState();
-    const [orders, setOrders] = useState([])
 
 
-    const handleChange = () => {
-        setChecked(!checked);
-        const delivery = {
-
+    const handleClick = (method) => {
+        if (method === 'delete') {
+            deleteOrder(order).then(setOrders);
         }
     }
+    return (
+        <div>
+                <div className="card">
+                    <div className="card-header">
+                        <h4 className="card-title">Order Number: {order.id}</h4>
+                        <p className="card-text">Order Total: {order.total}</p>
+                        <p className="card-text">Date: {order.date}</p>
+                        <p className="card-text">Status: {order.status}</p>
+                        <p className="card-text">Delivery: {order.delivery}</p>
+                        <p className="card-text">Order Name: {order.nameOnCard}</p>
+                        <div>
+                            <button type="button" className="btn btn-warning">Update Order</button>
+                        <button type="button" onClick={() => handleClick('delete') }className="btn btn-danger">Delet Order</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
