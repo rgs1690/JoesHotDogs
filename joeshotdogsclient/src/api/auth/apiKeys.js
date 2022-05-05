e9d0fb871017ce7e03ebe91a29f3bb11605ff417
Grace Sutherland <rgs1690@gmail.com>
2022-05-04 20:53:17 -0500

    hide api key :)

--- joeshotdogsclient/src/api/auth/apiKeys.js
+++ joeshotdogsclient/src/api/auth/apiKeys.js
@@ -2,7 +2,7 @@
 
 
 const firebaseConfig = {
-    apiKey:"AIzaSyDUpY0R9mwsyTsQLZncvjz5rPCaMZy1tLk",
+    apiKey:process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
